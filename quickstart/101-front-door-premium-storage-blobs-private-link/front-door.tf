02645e13fe8ae3b374fce935f34152dac081e5f1
John Downs <john@johndowns.co.nz>
2023-01-25 13:48:08 +1300

    Update default query string caching behavior

--- quickstart/101-front-door-premium-storage-blobs-private-link/front-door.tf
+++ quickstart/101-front-door-premium-storage-blobs-private-link/front-door.tf
@@ -67,16 +67,17 @@ resource "azurerm_cdn_frontdoor_route" "my_route" {
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.my_origin_group.id
   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.my_blob_container_origin.id]
 
-  supported_protocols       = ["Http", "Https"]
-  patterns_to_match         = ["/*"]
-  forwarding_protocol       = "HttpsOnly"
-  link_to_default_domain    = true
-  https_redirect_enabled    = true
-  cdn_frontdoor_origin_path = local.front_door_origin_path
+  supported_protocols           = ["Http", "Https"]
+  patterns_to_match             = ["/*"]
+  forwarding_protocol           = "HttpsOnly"
+  link_to_default_domain        = true
+  https_redirect_enabled        = true
+  cdn_frontdoor_origin_path     = local.front_door_origin_path
+  query_string_caching_behavior = "UseQueryString"
 
   cdn_frontdoor_custom_domain_ids = [
     azurerm_cdn_frontdoor_custom_domain.my_custom_domain.id
-   ]
+  ]
 }
 
 resource "azurerm_cdn_frontdoor_custom_domain" "my_custom_domain" {
